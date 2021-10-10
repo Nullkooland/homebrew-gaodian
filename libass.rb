f9033afdb422a965008a865b96d93d8df7ee9f38
Goose Bomb <goose_bomb@outlook.com>
2021-10-11 03:40:51 +0800

    Source compile libass instead of using bottle

--- libass.rb
+++ libass.rb
@@ -4,17 +4,25 @@ class Libass < Formula
   url "https://github.com/libass/libass/releases/download/0.15.2/libass-0.15.2.tar.xz"
   sha256 "1be2df9c4485a57d78bb18c0a8ed157bc87a5a8dd48c661961c625cb112832fd"
   license "ISC"
+  head "https://github.com/libass/libass.git"
 
-  bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "03a65a8977885e70b3072ed5b3cbb51178a140107932715f47fee5ee5b574d83"
-    sha256 cellar: :any,                 big_sur:       "d11e2972aa5ae2e763cf4101712fac61421082db4d47720a8adc17b8a3e84a27"
-    sha256 cellar: :any,                 catalina:      "b5f4cf1923f523dcc6faccd7d886c9677555c99d74f0bba2ea6f7aeaccd5511a"
-    sha256 cellar: :any,                 mojave:        "046012d55a33f84e483ac1e080c5698e0b2040b6385b6806ca7867226b91654f"
-  end
+  depends_on "nasm" => :build
+  depends_on "pkg-config" => :build
 
   depends_on "freetype"
   depends_on "fribidi"
   depends_on "goose-bomb/gaodian/harfbuzz"
+  
+  def install
+    args = %W[
+      --disable-fontconfig
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+  end
 
   test do
     (testpath/"test.cpp").write <<~EOS
