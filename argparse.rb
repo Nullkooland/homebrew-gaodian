a5ab4015f18f4528e416931a1ff50d756750547a
Goose Bomb <goose_bomb@outlook.com>
2022-04-08 13:43:24 +0800

    Update argparse to v2.3.0

--- argparse.rb
+++ argparse.rb
@@ -1,8 +1,8 @@
 class Argparse < Formula
   desc "Single header C++ argparse library"
   homepage "https://github.com/p-ranav/argparse"
-  url "https://github.com/p-ranav/argparse/archive/refs/tags/v2.2.tar.gz"
-  sha256 "f0fc6ab7e70ac24856c160f44ebb0dd79dc1f7f4a614ee2810d42bb73799872b"
+  url "https://github.com/p-ranav/argparse/archive/refs/tags/v2.3.tar.gz"
+  sha256 "6895d0f30d250ebb58bd93c1f07dbbcd0234216cb99ccc3ad211aa769bc5cf43"
   license "MIT"
   head "https://github.com/p-ranav/argparse.git"
 
