fcc449d490450973d36d79aa85747ba14000298c
Goose Bomb <goose_bomb@outlook.com>
2022-10-05 11:16:37 +0800

    Update SVT-AV1 to v1.2.1

--- svt-av1.rb
+++ svt-av1.rb
@@ -1,8 +1,8 @@
 class SvtAv1 < Formula
   desc "The Scalable Video Technology for AV1 (SVT-AV1 Encoder and Decoder)"
   homepage "https://gitlab.com/AOMediaCodec/SVT-AV1"
-  url "https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v1.1.0/SVT-AV1-v1.1.0.tar.gz"
-  sha256 "1c211b944ac83ef013fe91aee96c01289da4e3762c1e2f265449f3a964f8e4bc"
+  url "https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v1.2.1/SVT-AV1-v1.2.1.tar.gz"
+  sha256 "5c2466eb2990fa045b29c63b92a2b5f13d9cb841709df5e7206f96a59e119fdd"
   license "BSD-3-clause-clear"
   head "https://gitlab.com/AOMediaCodec/SVT-AV1.git"
 
@@ -16,8 +16,9 @@ class SvtAv1 < Formula
       -D CMAKE_BUILD_TYPE=Release
       -D BUILD_SHARED_LIBS=ON
       -D ENABLE_NASM=ON
-      -D BUILD_DEC=OFF
       -D BUILD_TESTING=OFF
+      -D BUILD_ENC=ON
+      -D BUILD_DEC=OFF
       -D BUILD_APPS=OFF
     ]
 
